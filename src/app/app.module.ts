b722966a76a0e7622eb7c021a98b6a12b6cacc57
sanshanthan <sanshanthan@yahoo.com>
2019-11-28 11:45:56 -0800

    Components,routes,navbar Inti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SectionSalesComponent } from './section-sales/section-sales.component';
+import { SectionOrdersComponent } from './section-orders/section-orders.component';
+import { SectionHealthComponent } from './section-health/section-health.component';
+
+import { appRoutes } from 'src/routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    NavbarComponent
+    NavbarComponent,
+    SectionSalesComponent,
+    SectionOrdersComponent,
+    SectionHealthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
